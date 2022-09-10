a495f2b972999f67f009f8b944416f9dc4ebef4c
munalaerastus@gmail.com <munalaerastus@gmail.com>
2022-09-10 09:40:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#Include <stdio.h>
+/**
+*main: prints teh string in the put function
+*
+*description: how to use the main function
+*this program prints the line ""Programming is like building a multilingual puzzle"
+*returns: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	returns (0);
+}
