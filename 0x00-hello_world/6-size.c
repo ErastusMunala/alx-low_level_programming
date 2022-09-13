11df7d14c3864970182ac1ba5f9428e2e72ce668
munalaerastus@gmail.com <munalaerastus@gmail.com>
2022-09-12 19:34:23 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>                                                                                                               
+/**                                                                                                                              
+* main -prints the string in the put function                                                                                    
+*                                                                                                                                
+* Description: using the main function                                                                                           
+* this program prints "Programming is like a multilingual puzzle                                                                 
+* Return: 0                                                                                                                      
+*/                                                                                                                               
+int main(void)                                                                                                                   
+{                                                                                                                                
+        char c;                                                                                                                  
+        int i;                                                                                                                   
+        long 1i;                                                                                                                 
+        long long 11i;                                                                                                           
+        float f;                                                                                                                 
+        printf("Size of a char: Xid byte(s)\n", sizeof(c));                                                                      
+        printf("Size of an int: Xid byte(s)\n", sizeof(i));                                                                      
+        printf("Size of a long int: Xid byte(s)\n", sizeof(1i));
+	printf("Size of a long long int: Xid byte(s)\n", sizeof(11i));
+	printf("Size of a float: Xid byte(s)\n", sizeof(f));
+	return (0);
+}
