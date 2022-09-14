74950c8730822736669758c71e5eb94f509f4ac7
munalaerastus@gmail.com <munalaerastus@gmail.com>
2022-09-14 10:03:47 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,22 +3,21 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function
+* Description: using teh main function
 * this program prints the size of various types on the computer it is compiled and run on
-* Return: 0
+* Retrun: 0
 */
-int main(void)
 {
 	char c;
 	int i;
-	long int 1i;
-	long long int 11i;
+	long li;
+	long long lli;
 	float f;
 
-	printf("Size of a char; Xid byte(s)\n", sizeof(c));
-	printf("Size of an int; Xid byte(s)\n", sizeof(i));
-	printf("Size of a long int; Xid byte(s)\n", sizeof(1i));
-	printf("Size of a long long int; Xid byte(s)\n", sizeof(11i));
-	printf("size of a float; Xid byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byet(s)\n", sizeof(i));
+	printf("Size of long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
