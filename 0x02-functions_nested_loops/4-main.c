0ce8ae97deee454795630cb46d4407d54050c5da
ErastusMunala <munalaerastus@gmail.com>
2022-12-12 16:25:06 -0800

    Checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('0');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (o);
+}
